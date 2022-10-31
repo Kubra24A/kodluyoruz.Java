4923f87edb9e7ac92088dc11fb6bff1a3fac7949
Kübra Akgül <113041850+Kubra24A@users.noreply.github.com>
2022-10-31 17:54:36 +0200

    Dairenin alanı ve çevresini hesaplayan progrem

--- src/daire.java
+++ src/daire.java
@@ -4,4 +4,31 @@ import java.util.Scanner ;
 
 public class daire {
     
+    /*
+    Daire alanı ve çevresini hesaplayan program
+    */
+   public static void main (String [] args) {
+
+       int r;
+
+       int aci;
+       double toplamAlan;
+
+       double pi=3.14 ;
+
+       Scanner inp = new Scanner (System.in);
+
+       System.out.println("Yarıçap uzunluğunu giriniz :");
+       r = inp.nextInt();
+
+       System.out.println("Merkezi açının ölçünü giriniz :");
+       aci = inp.nextInt();
+
+
+       toplamAlan = (pi*(r*r)*aci)/360 ;
+       System.out.println("Dairenin dilimin alanı :" + toplamAlan);
+
+   }
+   
+    
 }
