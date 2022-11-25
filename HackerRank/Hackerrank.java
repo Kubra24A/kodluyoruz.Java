2f2c83aebed463c93f091de1548f9dc70b7b9d45
Kübra Akgül <113041850+Kubra24A@users.noreply.github.com>
2022-11-25 22:25:39 +0300

    HackerRank

---
+++ HackerRank/Hackerrank.java
@@ -0,0 +1,17 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class Hackerrank {
+    public static void main (String[] args){
+
+        Scanner scan = new Scanner (System.in);
+
+        for(int i=0;i<3; i++){
+            int a = scan.nextInt();
+            System.out.println(a);
+        }
+
+    }
+    
+}
